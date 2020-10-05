2e365f4c64a47aed2f864c8050b673ff13318cd2
sn.jn <sn.jn@telia.com>
2020-10-05 17:10:03 +0200

    Chain, iterate through links

--- include/KinematicsClasses.h
+++ include/KinematicsClasses.h
@@ -6,47 +6,105 @@
 namespace ROBOTICS_LAB
 {
 
-class Joint;
-class Link;
-class Chain;
+struct Joint;
+struct Link;
+struct Chain;
 
-
-
-class Joint
+struct Joint
 {
     std::string name;
     Vec4 axis_angle;
-    struct parent
+    struct
     {
         Link* link;
         HomogenousTransform pose_offset;
-    };
+    }parent;
+
+    Joint(): Joint("joint-name"){}
+    Joint(std::string name);
 };
 
-class Link
+struct Link
 {
     std::string name;
     Joint* parent_joint;
     Joint* child_joint;
+    Link(): Link("link-name"){}
+    Link(std::string name);
+
 };
 
-class Chain
+struct Chain
 {
     std::string name;
     Link base_link;
-    struct children
+    struct
     {
         int count;
         Joint* joints_array; // = new Joints[chain.children.count]; 
         Link* links_array; // = new Joints[chain.children.count];
-    };
+    }children;
+    Chain() :Chain("chain", 1) {}
+    Chain(std::string name, int num_children);
+    ~Chain();
 };
+Link* get_next_link(Chain& chain, Link* current_link);
+
+//HomogenousTransform calculate_pose(Link* current_link);
 
 };
 
 //#define KINEMATICS_CLASSES_IMPLEMENTATION //Define this before the include in the file you want the implementation
 #ifdef KINEMATICS_CLASSES_IMPLEMENTATION
-    
+
+ROBOTICS_LAB::Joint::Joint(std::string name):name(name), axis_angle{1, 0, 0, PI/2}, parent{nullptr, {0}}{}
+ROBOTICS_LAB::Link::Link(std::string name):name(name), parent_joint(nullptr), child_joint(nullptr){}
+
+ROBOTICS_LAB::Chain::Chain(std::string name, int num_children) : name(name), base_link("base-link"), children{num_children, new Joint[num_children], new Link[num_children]}
+{
+    for(int i = 0; i < this->children.count; i++)
+    {
+
+    }
+}
+
+ROBOTICS_LAB::Chain::~Chain()
+{
+    delete[] this->children.joints_array;
+    delete[] this->children.links_array;
+}
+
+ROBOTICS_LAB::Link* ROBOTICS_LAB::get_next_link(Chain& chain, Link* current_link)
+{
+    int i = 0;
+    if(&chain.base_link != current_link)
+    {
+        for(i = 0; i < chain.children.count; i++)
+        {
+            if(&chain.children.links_array[i] == current_link)
+            {
+                i++;
+                break;
+            }
+        }
+        
+    }
+
+    if(i < chain.children.count)
+    {
+        return &chain.children.links_array[i];//Next link element in array
+    }
+    else
+    {
+        return current_link;
+    }
+}
+
+/*HomogenousTransform ROBOTICS_LAB::calculate_pose(Link* current_link)
+{
+
+}*/
+
 #endif
 
 
--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -6,9 +6,34 @@
 #define KINEMATICS_CLASSES_IMPLEMENTATION
 #include "KinematicsClasses.h"
 
+#include <iostream>
 
 int main()
 {
+    std::cout << "Lab2" << "\n";
+    ROBOTICS_LAB::Chain chain("Chain", 3);
+    //chain.children.count = 3;
+    //chain.children.links_array = new ROBOTICS_LAB::Link[chain.children.count];
+    chain.children.links_array[0].name = std::string("Link-1");
+    chain.children.links_array[1].name = std::string("Link-2");
+    chain.children.links_array[2].name = std::string("Link-3");
+
+    //chain.children.joints_array = new ROBOTICS_LAB::Joint[chain.children.count];
+    chain.children.joints_array[0].name = std::string("Joint-1");
+    chain.children.joints_array[1].name = std::string("Joint-2");
+    chain.children.joints_array[2].name = std::string("Joint-3");
+
+    ROBOTICS_LAB::Link* current_link = nullptr;
+    ROBOTICS_LAB::Link* next_link = &chain.base_link;
+    while(current_link != next_link)
+    {
+        current_link = next_link;
+        std::cout << current_link->name << "\n";
+        //ROBOTICS_LAB::HomogenousTransform transform = ROBOTICS_LAB::calculate_pose(current_link);
+
+
+        next_link = ROBOTICS_LAB::get_next_link(chain, current_link);
+    }
 
     return 0;
 }
